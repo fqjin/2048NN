2f15cb26f058115aa6f3cab89997786a7eef1ee1
Felix Jin <41841012+fqjin@users.noreply.github.com>
2018-11-17 19:12:45 -0500

    Reworked
    Explicit importing from board
    Windows curses implemented
    Removed draw_curses (not needed)

--- manual.py
+++ manual.py
@@ -1,58 +1,17 @@
-from board import *
-# includes SIZE, DIMENSIONS, SIZE_SQRD
-# includes Board class
-# includes randrange, randint, numpy as np
-
-def play_manual():
-    """Play 2048 manually with arrow keys"""
-    import curses
-    screen = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    screen.keypad(True)
-    try:
-        
-        def draw_curses(self,screen):
-            screen.erase()
-            screen.addstr(str(2**self.board).replace('1.',' .',SIZE_SQRD))
-            screen.addstr('\n Score : {}'.format(self.score))
-            
-        Board.draw_curses = draw_curses
-        game = Board()
-        game.generate_tile()
-        game.generate_tile()
-        game.draw_curses(screen)
-
-        while True:
-            char = screen.getch()
-            i = 0
-            if char == ord('q'): break
-            elif char == curses.KEY_LEFT: i = 1
-            elif char == curses.KEY_UP:   i = 2
-            elif char == curses.KEY_RIGHT:i = 3
-            elif char == curses.KEY_DOWN: i = 4
-            if i:
-                if game.moves[i-1]():
-                    game.generate_tile()
-                    game.draw_curses(screen)
-                    
-    finally:  # cleanup curses
-        curses.nocbreak()
-        screen.keypad(False)
-        curses.echo()
-        curses.endwin()
-        print('Game Over')
-        game.draw()
+import curses  # pip install windows-curses
+from board import Board
 
 
-def play_fixed(press_enter = False):
-    """ Run 2048 with the fixed move priority L,U,R,D.
-        press_enter (bool) : Defaults to False
+def play_fixed(press_enter=False):
+    """Run 2048 with the fixed move priority L,U,R,D.
+
+    Args:
+        press_enter (bool): Whether keyboard press is
+            required for each step. Defaults to False.
+            Type 'q' to quit when press_enter is True.
+
     """
-    game = Board()
-    game.generate_tile()
-    game.generate_tile()
-    game.draw()
+    game = Board(gen=True)
     while True:
         if press_enter and input() == 'q':
             break
@@ -64,3 +23,38 @@ def play_fixed(press_enter = False):
         else:
             print('Game Over')
             break
+
+
+def play_manual():
+    """Play 2048 manually with the arrow keys"""
+    screen = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    screen.keypad(True)
+
+    try:
+        # draw_curses function with screen.addstr not needed
+        game = Board(gen=True)
+        while True:
+            char = screen.getch()
+            i = -1
+            if char == ord('q'): break
+            elif char == curses.KEY_LEFT: i = 0
+            elif char == curses.KEY_UP:   i = 1
+            elif char == curses.KEY_RIGHT:i = 2
+            elif char == curses.KEY_DOWN: i = 3
+            if i != -1:
+                if game.moves[i]():
+                    game.generate_tile()
+                    game.draw()
+
+    finally:  # cleanup curses
+        curses.nocbreak()
+        screen.keypad(False)
+        curses.echo()
+        curses.endwin()
+        print('Game Over')
+
+
+if __name__ == '__main__':
+    play_manual()
