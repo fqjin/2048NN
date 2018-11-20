57787c358dab11309500095d72215827f59203f5
Felix Jin <41841012+fqjin@users.noreply.github.com>
2018-11-20 08:14:35 -0500

    Reworked
    Replaced method parameter with move_order parameter. Improved docstrings. Updated move function. Simplified some code.

--- mcts.py
+++ mcts.py
@@ -1,81 +1,76 @@
-from board import *
-# includes SIZE, DIMENSIONS, SIZE_SQRD
-# includes Board class
-# includes randrange, randint, numpy as np
+import numpy as np
+from board import Board
 
 
-def method_fixed(board):
-    """Returns L,U,R,D move priority as a tuple"""
-    return (0,1,2,3)
-    
+def mcts_fixed(game, move_order=(0, 1, 2, 3), number=5):
+    """Run tree search using a fixed move order for generating lines
 
-def mcts(game, method = method_fixed, number = 5):
-    """
-    Run Monte Carlo Tree Search
-    
     Args:
         game (Board): the starting game state
-        method: method for selecting moves. Default is method_fixed
-        number (int): # of lines to try for each move. Default is 5
+        move_order: tuple of the 4 move indices in order.
+            Defaults to (0, 1, 2, 3)
+        number (int): # of lines to try for each move.
+            Defaults to 5
+
     Returns:
-        scores for each move as a list [Left, Up, Right, Down]
-        
+        list: score increase for each move [Left, Up, Right, Down]
+
     """
-    # With a neural network, it may be more efficient to pass mulitple boards simultaneously
-    original_board = np.copy(game.board)
-    original_score = game.score
-    scores_list = [0,0,0,0]
-    
+    original = game.copy()
+    scores = [0, 0, 0, 0]
+
     for i in range(4):
-        if not game.moves[i]():
-            scores_list[i] = original_score
-            game.restore(original_board, original_score)
+        if not game.move(i):
+            # scores[i] = 0 or original_score
+            # game.restore(original.board, original.score)
+            # I think I can pass since moved=False nothing changed
+            # TODO: Verify this and simplify if-else
+            pass
         else:
             for _ in range(number):
-                game.restore(original_board, original_score)
-                game.moves[i]()
+                game.restore(original.board, original.score)
+                game.move(i)
                 game.generate_tile()
                 while True:
-                    move_order = method(game.board)
-                    for j in range(4):
-                        if game.moves[move_order[j]]():
+                    for j in move_order:
+                        if game.move(j):
                             game.generate_tile()
                             break
-                    else:        
+                    else:
                         # print('Game Over')
                         # game.draw()
                         break
-                scores_list[i] += game.score       
-            scores_list[i] /= number  # Calculate average final score
-            game.restore(original_board, original_score)
-            
-    return scores_list
-    
-    
-def play_mcts(game, number = 5):
-    """ 
-    Play a game using the default mcts
+                scores[i] += game.score
+            scores[i] /= number  # Calculate average final score
+            scores[i] -= original.score
+            game.restore(original.board, original.score)
+
+    return scores
+
+
+def play_mcts_fixed(game, move_order=(0, 1, 2, 3), number=5, verbose=False):
+    """Play a game using the default mcts_fixed
+
     Args:
         game (Board): the starting game state
-        number (int): Default is 5
+        move_order: tuple of the 4 move indices in order.
+            Defaults to (0, 1, 2, 3)
+        number (int): # of lines to try for each move.
+            Defaults to 5
+        verbose (bool): whether to print mcts scores
+            Defaults to False
+
     """
     while True:
-        scores_list = mcts(game, number = number)
-        # print(scores_list)
-        for i in np.flipud(np.argsort(scores_list)):
-            if game.moves[i]():
+        scores = mcts_fixed(game, move_order, number)
+        if verbose:
+            print(scores)
+        for i in np.flipud(np.argsort(scores)):
+            if game.move(i):
                 game.generate_tile()
                 game.draw()
                 break
-        else:        
+        else:
             print('Game Over')
             break
-
-
-# FOR TESTING
-print('a = Board()')
-a = Board()
-a.generate_tile()
-a.generate_tile()
-a.draw()
 
